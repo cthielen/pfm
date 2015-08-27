6a29263355c9c5f3a6e3ef370f21a352b1265a4a
Christopher Thielen <cthielen@gmail.com>
2015-08-27 11:36:17 -0700

    Add type to Account model (e.g. Checking, Savings, Loan)

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108003657) do
+ActiveRecord::Schema.define(version: 20150827183345) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "type"
   end
 
   create_table "budgets", force: :cascade do |t|
